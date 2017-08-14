e1c8aa4f60077fcef646cdefdfa26c957b20d4e6
victorlhlee <victorlhlee@users.noreply.github.com>
2017-08-13 14:59:07 -1000

    update ex 6

--- solutions.js
+++ solutions.js
@@ -122,7 +122,7 @@ console.log("quotient: " , quotient);
  *   @param Datatype: Number `x`
  *   @return Datatype: String
  *
- * This function will return the string "Mariah Carey has been married `x` amount of times." Where `x` is the value when you invoke the function. Invoke this function using the variable `sum`. 
+ * This function will return the string "Mariah Carey has been married `x` amount of times." Where `x` is the value when you invoke the function. Invoke this function using the variable `daDiva`. 
  * Console.log your result.
 */
 
